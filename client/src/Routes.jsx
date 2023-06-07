0c721b5a1f78299e1f2256de7a1fd4a1fcfcb693
Duc Minh <Minh1411hn@Gmail.com>
2023-06-08 00:58:51 +0700

    loading screen

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,29 +1,47 @@
-import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
-import {useContext} from "react";
-import {UserContext} from "./UserContext.jsx";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserContext.jsx";
 import ResetPassword from "./ResetPassword.jsx";
 import MainSection from "./MainSection.jsx";
 import RedirectToOriginal from "./RedirectToOriginal.jsx";
-
-
+import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
 
 export default function Routes() {
-    const {email, id, username, resetPassword,avatar, resetPasswordMessage, shortenedId} = useContext(UserContext);
+    const {
+        email,
+        id,
+        username,
+        resetPassword,
+        avatar,
+        resetPasswordMessage,
+        shortenedId
+    } = useContext(UserContext);
+    const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     // Simulate loading time or perform async operations
+    //     setTimeout(() => {
+    //         setLoading(false);
+    //     }, 300);
+    // }, []);
+    //
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
 
     if (shortenedId) {
-        return <RedirectToOriginal/>;
+        return <RedirectToOriginal />;
     }
 
-
-    if ((email && id && username && !resetPassword)){
-        return <MainSection/>;
+    if (email && id && username && !resetPassword) {
+        return <MainSection />;
     }
 
-    if (resetPassword || resetPasswordMessage ) {
-        return <ResetPassword/>
+    if (resetPassword || resetPasswordMessage) {
+        return <ResetPassword />;
     }
 
-    return (
-        <RegisterAndLoginForm/>
-    );
+    if (!shortenedId && !email && !id && !username && !resetPassword && !resetPasswordMessage) {
+        return <RegisterAndLoginForm />;
+    }
+
 }
